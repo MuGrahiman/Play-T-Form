023a68b8ee98d6eb0096d988ceff7d747f573821
MuGrahiman <mujeebrahiman2000@gmail.com>
2023-12-24 08:48:59 +0530

    chore:create boiler plate for EJS and add basic style for body

--- Src/app.js
+++ Src/app.js
@@ -1,24 +1,26 @@
+const morgan = require("morgan");
 const express = require("express");
 const dotenv = require("dotenv");
-const bodyparser = require("body-parser");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
 
 dotenv.config();
 const app = express();
-
-app.use(express.static("Public"));
-app.use("/css", express.static(__dirname + "Public/CSS"));
-app.use("/img", express.static(__dirname + "Public/IMG"));
-app.use("/js", express.static(__dirname + "Public/JS"));
-
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
-app.set("views", "./Views");
+app.use(express.static("../Public"));
+app.use("/css", express.static(path.join(__dirname, "../Public/CSS")));
+app.use("/img", express.static(path.join(__dirname, "../Public/IMG")));
+app.use("/js", express.static(path.join(__dirname, "../Public/JS")));
+
+app.set("views", path.join(__dirname, "Views"));
+app.set("layout", path.join(__dirname, "Views/layout.ejs"));
 app.set("view engine", "ejs");
+app.use(expressLayouts);
 
 app.get("/", (req, res) => res.send("server working successfully"));
 
 const routes = require("./Routes/router");
 app.use("/api", routes);
-
-
 
 module.exports = app;
